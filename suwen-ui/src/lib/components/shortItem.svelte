099fa9d6c3771dfc1c0ec66fe062515e75f1cb36
amtoaer <amtoaer@gmail.com>
2025-08-16 16:02:35 +0800

    feat: 为图文部分引入 tooltip

--- suwen-ui/src/lib/components/shortItem.svelte
+++ suwen-ui/src/lib/components/shortItem.svelte
@@ -2,14 +2,14 @@
 	let { slug, title, image, width = 96, height = 96 } = $props();
 </script>
 
-<a class="w-24 h-24 rounded-lg overflow-hidden bg-gray-100" href={`/shorts/${slug}`}>
+<a href={`/shorts/${slug}`}>
 	<img
 		fetchpriority="high"
 		src={`https://amto.cc/cdn-cgi/image/width=100,height=100,fit=cover/${image}`}
 		alt={title}
 		{width}
 		{height}
-		class="w-full h-full object-cover sm:hover:scale-105 sm:transition-transform sm:duration-400 sm:ease-in-out"
+		class="w-full h-full object-cover sm:hover:scale-105 sm:transition-transform sm:duration-400 sm:ease-in-out rounded-lg"
 		draggable="false"
 	/>
 </a>
--- suwen-ui/src/lib/components/shorts.svelte
+++ suwen-ui/src/lib/components/shorts.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { ChevronRight, BookImage } from '@lucide/svelte';
+	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 	import ShortItem from './shortItem.svelte';
 
 	let { shorts } = $props();
@@ -11,11 +12,35 @@
 			<BookImage class="size-4" />
 			图文
 		</h2>
-		<ChevronRight class="w-5 h-5 text-gray-400" />
+		<Tooltip.Provider delayDuration={0}>
+			<Tooltip.Root>
+				<Tooltip.Trigger>
+					<a href="/shorts" class="text-sm text-gray-500 hover:underline">
+						<ChevronRight class="w-5 h-5 text-gray-400" />
+					</a>
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>更多图文</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</Tooltip.Provider>
 	</div>
-	<div class="flex gap-4">
+	<div class="grid grid-cols-4 sm:grid-cols-8 gap-2 sm:gap-4">
 		{#each shorts as short}
-			<ShortItem slug={short.slug} image={short.coverImages[0]} title={short.title} />
+			<Tooltip.Provider delayDuration={0}>
+				<Tooltip.Root>
+					<Tooltip.Trigger
+						><ShortItem
+							slug={short.slug}
+							image={short.coverImages[0]}
+							title={short.title}
+						/></Tooltip.Trigger
+					>
+					<Tooltip.Content>
+						<p>{short.title}</p>
+					</Tooltip.Content>
+				</Tooltip.Root>
+			</Tooltip.Provider>
 		{/each}
 	</div>
 </div>
